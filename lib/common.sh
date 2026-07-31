840f4c157a06d49bf48958a816901a52f14f980d
eloyvicent13-lgtm <eloyvicent13@gmail.com>
2026-07-31 23:50:53 +0200

    v1.3.5: blinda el rotulo de la barra principal frente al reset del subrayado decorativo

--- lib/common.sh
+++ lib/common.sh
@@ -5,7 +5,7 @@
 # Este archivo se carga con `source`, no se ejecuta directamente.
 
 WAISE_NAME="Waise Theme"
-WAISE_VERSION="1.3.4"
+WAISE_VERSION="1.3.5"
 
 # Repositorio usado por `waise upgrade`. Se puede sobrescribir exportando
 # WAISE_REPO_URL / WAISE_REPO_BRANCH antes de ejecutar el comando.
